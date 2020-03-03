1082fffdfea18d4e07f3800b8e4a7fdf3125c6cb
Cindy <chengxiwenhnu@163.com>
2020-03-03 22:35:29 +0800

    首页专题区域布局Topic及reducer的设计

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
-export const HomeWrapper = styled.div`
+export const HomeWrapper = styled.div `
 	overflow: hidden;
   width: 960px;
 	margin: 0 auto;
 `;
 
-export const HomeLeft = styled.div`
+export const HomeLeft = styled.div `
 	float: left;
 	margin-left: 15px;
 	padding-top: 30px;
@@ -17,7 +17,36 @@ export const HomeLeft = styled.div`
 	}
 `;
 
-export const HomeRight = styled.div`
+export const HomeRight = styled.div `
 	width: 240px;
 	float: right;
+`;
+
+export const TopicWrapper = styled.div `
+	${'' /* 触发BFC */}
+	overflow: hidden;
+	padding: 20px 0 10px 0;
+	${'' /* 将最外面一个拉回去，其他的向右拉18px */}
+	margin-left: -18px;
+`;
+
+export const TopicItem = styled.div `
+	float: left;
+	height: 32px;
+	line-height: 32px;
+	background: #f7f7f7;
+	font-size: 14px;
+	color: #000;
+	border: 1px solid #dcdcdc;
+	border-radius: 4px;
+	padding-right: 10px;
+	margin-left: 18px;
+	margin-bottom: 18px;
+	.topic-pic {
+		display: block;
+		float: left;
+		width: 32px;
+		height: 32px;
+		margin-right: 10px;
+	}
 `;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,9 +2,11 @@
 // 这样该函数生成的reducer里面的数据内容就是一个immutable的数据内容
 import { combineReducers } from 'redux-immutable';
 import { reducer as headerReducer } from '../common/header/store';
+import homeReducer from '../pages/home/store/reducer';
 
 const reducer = combineReducers({
-    header: headerReducer
+    header: headerReducer,
+    home: homeReducer
 })
 
 export default reducer;
