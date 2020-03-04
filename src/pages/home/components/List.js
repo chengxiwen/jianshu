46caf2dccf6f8db1dbe594f54d7337be55711f09
Cindy <chengxiwenhnu@163.com>
2020-03-04 16:06:20 +0800

    实现加载List更多功能

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListItem, ListInfo } from '../style';
+import { ListItem, ListInfo, LoadMore } from '../style';
+import { actionCreators } from '../store';
 
 class List extends Component {
 	render() {
-		const { list } = this.props;
+		const { list, getMoreList, page } = this.props;
 		return (
 			<div>
 				{
-					list.map((item) => {
+					list.map((item, index) => {
 						return(
-							<ListItem key={item.get('id')}>
+							<ListItem key={index}>
 								<img className="pic" alt="" src={item.get('imgUrl')}/>
 								<ListInfo>
 									<h3 className="title">{item.get('title')}</h3>
@@ -20,6 +21,7 @@ class List extends Component {
 						)
 					})
 				}
+				<LoadMore onClick={() => getMoreList(page)}>更多文字</LoadMore>
 			</div>
 		)
 	}
@@ -27,7 +29,14 @@ class List extends Component {
 
 const mapStateToProps = (state) => ({
 	// list: state.get('home').get('articleList')
-	list: state.getIn(['home','articleList'])
+	list: state.getIn(['home','articleList']),
+	page: state.getIn(['home','articlePage'])
 })
 
-export default connect(mapStateToProps,null)(List);
+const mapDispatchToProps = (dispatch) => ({
+	getMoreList(page) {
+		dispatch(actionCreators.getMoreList(page))
+	}
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(List);
